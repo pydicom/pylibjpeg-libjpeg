242f9ce76eaf9fbeecd71d7bfe3537d81b495a53
scaramallion <scaramallion@users.noreply.github.com>
2022-01-22 11:38:19 +1100

    Final cleanup

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ def get_mscv_args():
     flags = [
         '/GS',  # Buffer security check
         '/W3',  # Warning level
-        #'/wd"4335"',  # Ignore warning 4335
         '/Zc:wchar_t',  # Use windows char type
         '/Zc:inline',  # Remove unreferenced function or data (...)
         '/Zc:forScope',
@@ -39,8 +38,6 @@ def get_mscv_args():
         '/D_UNICODE',
         '/DUNICODE',
     ]
-    # if sys.version_info.major == 3 and sys.version_info.minor >= 9:
-    #     flags.remove('/wd"4335"')
 
     # Set the architecture based on system architecture and Python
     is_x64 = platform.architecture()[0] == '64bit'
